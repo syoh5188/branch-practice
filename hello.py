2c5473b6f15afd5d1479397aa0a4d6dcecf8a3b4
syoh5188 <syoh5188@gmail.com>
2022-05-06 15:31:17 +0900

    move hello.py to cd ./branch-practice change hello.py and check year5.py

--- hello.py
+++ hello.py
@@ -1,3 +1,11 @@
-print('hello.py in branch-practice')
+print('hello.py from year-3')
 
+for i in range(1,20+1):
+	if (i % 3 == 0):
+		print('year')
+	elif (i % 5 == 0):
+		print('dream')
+	else:
+		print(f'i: {i}')
 
+print('End of hello.py')
---
+++ year5.py
@@ -0,0 +1,11 @@
+print('year-5.py in branch')
+
+for i in range(1,26):
+	if i % 5 == 0 :
+		print('dream')
+	elif i % 15 == 0 :
+		print('year5')
+	else:
+		print(f'i:{i}')
+
+print('END of year5.py')
